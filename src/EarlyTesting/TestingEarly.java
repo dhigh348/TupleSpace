ee3591f2163a6ecfefe89249735c90f4866267e0
Danan Q High <dqhigh@cs.unm.edu>
2018-10-03 11:20:40 -0600

    Adding naive solution along with the final solution run times.

--- src/EarlyTesting/TestingEarly.java
+++ src/EarlyTesting/TestingEarly.java
@@ -1,66 +1,68 @@
+package EarlyTesting;
+
 import java.util.ArrayList;
 
 /**
  * Test.java is the class that tests the program
  * Danan High, 9/16/2018
  */
-public class Test {
+public class TestingEarly {
 
     /**
      * main method to run the program
      * @param args to run the main program
      */
     public static void main(String[] args) {
-        TupleSpace ts = new TupleSpace(6);
-        Tuple tuple = new Tuple();
-        ArrayList<Tuple> tupleList = new ArrayList<>();
+        TupleSpaceEarly ts = new TupleSpaceEarly(6);
+        TupleEarly tuple = new TupleEarly();
+        ArrayList<TupleEarly> tupleList = new ArrayList<>();
 
         long time1 = System.currentTimeMillis();
         for (int i = 0; i < 100000; i++) {
             if (i % 10 == 0) {
-                tuple = new Tuple("anna",
+                tuple = new TupleEarly("anna",
                                   "filling good",
                                   "Way to go");
             } else if (i % 10 == 1) {
-                tuple = new Tuple("anna");
+                tuple = new TupleEarly("anna");
             } else if (i % 10 == 2) {
-                tuple = new Tuple("anna",
+                tuple = new TupleEarly("anna",
                                   "cool",
                                   "why");
             } else if (i % 10 == 3) {
-                tuple = new Tuple("anna",
+                tuple = new TupleEarly("anna",
                                   "abc",
                                   true);
             } else if (i % 10 == 4) {
-                tuple = new Tuple("anna",
+                tuple = new TupleEarly("anna",
                                   "abc" + i,
                                   true,
                                   "Hi hows it going");
             } else if (i % 10 == 5) {
-                tuple = new Tuple("cole",
+                tuple = new TupleEarly("cole",
                                   "abc",
                                   false,
                                   "Hi hows it going");
             } else if (i % 10 == 6) {
-                tuple = new Tuple("sheela" + i,
+                tuple = new TupleEarly("sheela" + i,
                                   "abc",
                                   true,
                                   "Hi hows it going");
             } else if (i % 10 == 7) {
-                tuple = new Tuple("anna",
+                tuple = new TupleEarly("anna",
                                   "abc",
                                   true,
                                   " why",
                                   8);
             } else if (i % 10 == 8) {
-                tuple = new Tuple("bill" + i,
+                tuple = new TupleEarly("bill" + i,
                                   "abc",
                                   13.56,
                                   "Hi hows it going" + i,
                                   "yellow",
                                   101);
             } else if (i % 10 == 9) {
-                tuple = new Tuple("bob",
+                tuple = new TupleEarly("bob",
                                   "abc" + i,
                                   123458679,
                                   "Hi hows it going",
@@ -69,37 +71,34 @@ public class Test {
             ts.add(tuple);
         }
         System.out.println("After Add: " +
-                               (System.currentTimeMillis() - time1) + "\n");
-        
-        
-        
+                                   (System.currentTimeMillis() - time1) + "\n");
+
+
+
         long time2 = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             ts.read("*", "abc", "*", "*");
-//            tuple.printTuple();
-//            System.out.println(tuple + "\n");
         }
         System.out.println("After Read: " +
-                               (System.currentTimeMillis() - time2) + "\n");
-        
-        
-        
-        
+                                   (System.currentTimeMillis() - time2) + "\n");
+
+
+
+
         long time3 = System.currentTimeMillis();
         for (int i = 0; i < 10000; i++) {
             tuple = ts.remove("*", "*", "*", "Hi hows it going");
             if (tuple != null) {
                 tupleList.add(tuple);
-//                tuple.printTuple();
-//                System.out.println(tuple.printTuple());
             }
         }
-        
-        for (Tuple t: tupleList) {
+
+        for (TupleEarly t: tupleList) {
             ts.add(t);
         }
         System.out.println("After Remove: " +
-                               (System.currentTimeMillis() - time3) + "\n");
+                                   (System.currentTimeMillis() - time3) + "\n");
     }
 
 }
+
---
+++ src/EarlyTesting/TupleSpaceEarly.java
@@ -0,0 +1,89 @@
+package EarlyTesting;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/**
+ * TupleSpace.java is the data center for holding the users and their associated
+ *   messages and statuses.
+ * Danan High, 9/18/2018
+ */
+public class TupleSpaceEarly {
+
+    private int size;
+    private List<TupleEarly> repository;
+
+    /**
+     * Constructor for the TupleSpace
+     */
+    public TupleSpaceEarly(int size) {
+        this.size = size;
+        this.repository = new LinkedList<>();
+    }
+
+    /**
+     * Adding the specified tuple to the TupleSpace repository
+     */
+    public void add(TupleEarly tuple) {
+        if (tuple != null) {
+            this.repository.add(tuple);
+        }
+    }
+
+
+    /**
+     * Returning and removing the tuple from the repository.
+     * @return tuple from the repository
+     */
+    public TupleEarly remove(Object...objects) {
+        TupleEarly tuple = null;
+
+        for (TupleEarly t: repository) {
+            if (t.getSize() == objects.length) {
+                if (t.checkPattern(objects)) {
+                    tuple = t;
+                }
+            }
+        }
+
+        if (tuple != null) {
+            repository.remove(tuple);
+        }
+
+        return tuple;
+    }
+
+
+    /**
+     * Returning the individual tuple from the repository
+     * @return tuple to print
+     */
+    public TupleEarly read(Object...objects) {
+        TupleEarly tuple = null;
+
+        for (TupleEarly t: repository) {
+            if (t.getSize() == objects.length) {
+                if (t.checkPattern(objects)) {
+                    tuple = t;
+                }
+            }
+        }
+
+        return tuple;
+    }
+
+
+    /**
+     * Printing the map
+     */
+    public void print(Object...objects) {
+        TupleEarly tuple = null;
+        ArrayList<TupleEarly> list = new ArrayList<>();
+
+
+    }
+
+}
+
